d1d2b8f12256fe67100180dbc9ac953967711d59
Ryan McWhorter <rmcwhorter@gmail.com>
2021-01-29 17:38:07 -0600

    Final update of HW01

---
+++ HW01/Initials.py
@@ -0,0 +1,27 @@
+# File: Initials.py
+# Student: Ryan McWhorter
+# UT EID: rcm3369
+# Course: CS303E
+# 
+# Date Created: 21 Jan 2021
+# Date Last Modified: 29 Jan 2021
+# Description of Program: The first homework for CS303E, it prints out my initials.
+
+
+# Ryan Christopher McWhorter
+outstring = """
+  RRRRRRRRRRRR      CCCCCCCCCCCC      MMMM    MMMM      
+  RRRRRRRRRRRR      CCCCCCCCCCCC      MMMMM  MMMMM      
+  RRR      RRR      CCC      CCC      MMMMMMMMMMMM      
+  RRR      RRR      CCC      CCC      MMM MMMM MMM      
+  RRR      RRR      CCC               MMM  MM  MMM      
+  RRRRRRRRR         CCC               MMM      MMM      
+  RRRRRRRRR         CCC               MMM      MMM      
+  RRR    RRR        CCC      CCC      MMM      MMM      
+  RRR     RRR   ..  CCCCCCCCCCCC  ..  MMM      MMM  ..  
+  RRR      RRR  ..  CCCCCCCCCCCC  ..  MMM      MMM  ..  
+"""
+
+print()
+print(outstring)
+print()
